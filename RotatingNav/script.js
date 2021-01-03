7524ad2b27b075495a28b29167f92c98663989c4
Kyle McKell <46072760+KyleMcKell@users.noreply.github.com>
2021-01-03 11:51:32 -0700

    changed the event listeners to a function which decreased the amount of code needed to accomplish the same task

--- RotatingNav/script.js
+++ RotatingNav/script.js
@@ -4,25 +4,29 @@ const content = document.querySelector(".content");
 const navbar = document.querySelector(".navbar");
 
 openNavBtn.addEventListener("click", () => {
-	openNavBtn.classList.remove("active");
-	closeNavBtn.classList.add("active");
-	openNavBtn.disabled = true;
-	closeNavBtn.disabled = false;
-	content.classList.remove("hide-nav");
-	content.classList.add("show-nav");
-
-	navbar.classList.remove("hide-nav");
-	navbar.classList.add("show-nav");
+	handleClick(openNavBtn, closeNavBtn, true);
 });
 
 closeNavBtn.addEventListener("click", () => {
-	openNavBtn.classList.add("active");
-	closeNavBtn.classList.remove("active");
-	closeNavBtn.disabled = true;
-	openNavBtn.disabled = false;
-	content.classList.add("hide-nav");
-	content.classList.remove("show-nav");
-
-	navbar.classList.remove("show-nav");
-	navbar.classList.add("hide-nav");
+	handleClick(closeNavBtn, openNavBtn, false);
 });
+
+const handleClick = (clickedButton, hiddenButton, navOpen) => {
+	clickedButton.classList.remove("active");
+	hiddenButton.classList.add("active");
+
+	clickedButton.disabled = true;
+	hiddenButton.disabled = false;
+
+	if (navOpen) {
+		content.classList.add("show-nav");
+		navbar.classList.add("show-nav");
+		content.classList.remove("hide-nav");
+		navbar.classList.remove("hide-nav");
+	} else {
+		content.classList.add("hide-nav");
+		navbar.classList.add("hide-nav");
+		content.classList.remove("show-nav");
+		navbar.classList.remove("show-nav");
+	}
+};
