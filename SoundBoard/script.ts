1347a430d852dd16ab60d2ad9b604d7fb3e9201b
Kyle McKell <46072760+KyleMcKell@users.noreply.github.com>
2021-01-07 13:10:43 -0700

    added logic to play sounds

--- SoundBoard/script.ts
+++ SoundBoard/script.ts
@@ -1,1 +1,16 @@
-console.log("hello world");
+const audioButtons = document.querySelectorAll<HTMLButtonElement>(
+	".sound-button"
+);
+const audios = document.querySelectorAll<HTMLAudioElement>("audio");
+
+// loops through audio buttons, finds the index of the button in the nodelist
+// sets that index to the audios node list
+// to find the corresponding audio, then plays it on click
+audioButtons.forEach((button: HTMLButtonElement) => {
+	const audio = audios[Array.prototype.indexOf.call(audioButtons, button)]!;
+
+	button.addEventListener("click", () => {
+		audio.currentTime = 0;
+		audio.play();
+	});
+});
