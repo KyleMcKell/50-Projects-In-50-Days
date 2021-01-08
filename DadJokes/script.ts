5da011716e66bdda043319ff19c92eb5de530923
Kyle McKell <46072760+KyleMcKell@users.noreply.github.com>
2021-01-07 19:18:32 -0700

    added fetch functionality and event listener to button to update the joke whenever it is pressed

--- DadJokes/script.ts
+++ DadJokes/script.ts
@@ -1,1 +1,25 @@
-console.log("hello world");
+const getJoke = async () => {
+	const res = await fetch("https://icanhazdadjoke.com/", {
+		method: "GET",
+		headers: {
+			Accept: "application/json",
+		},
+	});
+
+	if (!res.ok) {
+		throw new Error(`Error! Status: ${res.status}`);
+	} else {
+		const data = await res.json();
+		const joke = await data.joke;
+		jokeText.innerText = joke;
+	}
+};
+
+const jokeButton = document.querySelector<HTMLButtonElement>(".joke-button")!;
+const jokeText = document.querySelector<HTMLParagraphElement>(".joke-text")!;
+
+jokeButton.addEventListener("click", () => {
+	getJoke();
+});
+
+getJoke();
