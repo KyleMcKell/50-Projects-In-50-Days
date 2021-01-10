5d1a90ca15cca913839182f1ab6140e433ef166c
Kyle McKell <46072760+KyleMcKell@users.noreply.github.com>
2021-01-09 17:29:12 -0700

    added logic to show the respective keycodes

--- EventKeyCodes/script.ts
+++ EventKeyCodes/script.ts
@@ -1,1 +1,18 @@
-console.log("hello world");
+const eventKey = document.querySelector<HTMLDivElement>(".event-key")!;
+const eventKeyCode = document.querySelector<HTMLDivElement>(".event-keycode")!;
+const eventCode = document.querySelector<HTMLDivElement>(".event-code")!;
+
+const clickAKeyDiv = document.querySelector<HTMLDivElement>("#clickAKey")!;
+const keyClickedDiv = document.querySelector<HTMLDivElement>("#keyClicked")!;
+
+document.addEventListener("keydown", (key) => {
+	clickAKeyDiv.classList.add("hidden");
+	keyClickedDiv.classList.remove("hidden");
+	eventKey.innerText = key.key;
+	eventKeyCode.innerText = key.keyCode.toString();
+	eventCode.innerText = key.code;
+
+	if (key.key === " ") {
+		eventKey.innerText = '"     "';
+	}
+});
